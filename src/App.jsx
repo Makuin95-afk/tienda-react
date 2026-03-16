147f7d10947aa95a6f880026cbf6f8bcc3030a7b
Makuin95-afk <varelaoyuelaivanfernando@gmail.com>
2026-03-16 17:01:37 -0600

    test app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import Home from "./pages/Home";
-import ProductDetail from "./pages/ProductDetail";
-import Checkout from "./pages/Checkout";
-import Success from "./pages/Success";
-
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/producto/:id" element={<ProductDetail />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </div>
-  );
+  return <h1>MI TIENDA YA SE ACTUALIZÓ</h1>;
 }
 
 export default App;
